f44373f6118921c5702f495f1a09a49861e0f51a
Marko Oldenburg <marko.oldenburg@cooltux.net>
2021-03-27 16:41:18 +0100

    :-)

--- lib/FHEM/Tesla/Powerwall.pm
+++ lib/FHEM/Tesla/Powerwall.pm
@@ -392,7 +392,7 @@ sub Set {
     elsif ( lc $cmd eq 'setpassword' ) {
         return q{please set Attribut emailaddr first}
           if ( AttrVal( $name, 'emailaddr', 'none' ) eq 'none' );
-        return qq(usage: ${cmd} pass=<password>) if ( scalar( @{$aArg} ) != 2
+        return qq(usage: ${cmd} pass=<password>) if ( scalar( @{$aArg} ) != 0
                                                    || scalar(keys %{$hArg}) != 1 );
 
         StorePassword( $hash, $name, $hArg->{'pass'} );
